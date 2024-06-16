f972491d6f2e50c753b0e4224c4d3ca796a0230a
Erfan <codewithfed@gmail.com>
2024-06-16 22:15:39 +0330

    get rid of the fucntion duplication

--- index.js
+++ index.js
@@ -2,29 +2,9 @@ const searchbox = document.getElementById('input');
 const btn = document.getElementById('btn');
 const resultbox = document.getElementById('ReturnNum');
 searchbox.addEventListener('keypress', (event) => {
-    if (event.key === 'Enter') {
-        Calculatoin();
-    }
+    if (event.key === 'Enter') Calculatoin();
 })
-btn.addEventListener('click', () => {
-    if (searchbox.value != '') {
-
-        let R = Number(searchbox.value);
-        const P = Math.PI;
-        const circumferece = 2 * R * P;
-        const surface = (R ** 2) * P;
-        searchbox.value = '';
-        resultbox.classList.add('result__com');
-        resultbox.innerText = `Circumference= ${circumferece} 
-        Surface = ${surface}`;
-    }
-    else {
-        resultbox.classList.remove('result__com');
-        resultbox.innerText = 'Nothing is Passed!'
-    }
-}
-)
-// 
+btn.addEventListener('click', () => Calculatoin())
 function Calculatoin() {
     if (searchbox.value != '') {
         let R = Number(searchbox.value);
